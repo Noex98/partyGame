ef41eedc28b9f56212f8b52ac0a83391db15f548
Johannes <johannes.knickering@hotmail.com>
2023-05-17 00:14:03 +0200

    add player btn

--- src/components/AddPlayerBtn/AddPlayerBtn.tsx
+++ src/components/AddPlayerBtn/AddPlayerBtn.tsx
@@ -1,18 +1,47 @@
-import React from 'react';
+"use client";
+
+import React, { useRef, useState } from 'react';
 import styles from './style.module.scss';
+import Link from 'next/link';
 import { IoMdAddCircle } from 'react-icons/io'
 import { AiOutlineArrowRight } from 'react-icons/ai'
+import { usePlayerContext } from '@/Utils/PlayerContext';
 
 export const AddPlayerBtn = () => {
-  return (
-    <>
+
+    const playerContext = usePlayerContext()
+    const [inputValue, setInputValue] = useState("")
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    if(!playerContext) return null;
+
+    const addHandler = () => {
+        if(!!inputValue.length){
+            playerContext.setPlayers(prev => [...prev, inputValue]);
+            setInputValue("")
+        }
+        if(inputRef.current){
+            inputRef.current.focus()
+        }
+    }
+
+    return (
         <div className={styles.inputWrapper}>
-            <input className={styles.input} placeholder='Add player' type="text" />
+            <input 
+                ref={inputRef}
+                className={styles.input} placeholder='Add player' 
+                type="text" 
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)}
+            />
             <span className={styles.iconsContainer}>
-                <IoMdAddCircle />
-                <AiOutlineArrowRight />
+                <button onClick={e => addHandler()}>
+                    <IoMdAddCircle size={30}/>
+                </button>
+                <Link href="/game">
+                    <AiOutlineArrowRight size={30}/>
+                </Link>
             </span>
         </div>
-    </>
-  )
+    )
 }
